3bdf68f75bc6b9826dc177074a1dbb3cd944b0a1
CarlKaste <117178619+CarlKaste@users.noreply.github.com>
2022-11-23 20:42:07 +0100

    Added a lot
    New modells, effects, sounds, lighting

---
+++ Cabin in the woods/Assets/Cabin in the woods/Scripts/FirePlace.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePlace : MonoBehaviour
+{
+    [SerializeField]
+    private Light light;
+
+    [SerializeField]
+    private float lightIntensityMin;
+
+    [SerializeField]
+    private float lightIntensityMax;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lightValue = Random.Range(lightIntensityMin, lightIntensityMax);
+        light.intensity = lightValue;
+    }
+}
---
+++ Cabin in the woods/Assets/Cabin in the woods/Scripts/RainTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource outdoorRainSound;
+
+    [SerializeField]
+    private AudioSource indoorRainSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            outdoorRainSound.Stop();
+            indoorRainSound.Play();
+        }
+        else
+        {
+            outdoorRainSound.Play();
+            indoorRainSound.Stop();
+        }
+    }
+}
